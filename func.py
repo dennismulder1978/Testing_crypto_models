6271b7267fdcca6802ade08d6d9990842a6fd205
Dennis <dnns.mldr@gmail.com>
2023-12-21 19:38:06 +0100

    step 1 and part 2

--- func.py
+++ func.py
@@ -1,12 +1,18 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import os
 
-def create_arrays(coin_array, N_STEPS):
+def create_arrays(file_name, N_STEPS):
     """
     Creates a numpy array with multiple N_STEPS-hours cohorts of 4 trade values of a cryptocoin. 
     Returns:
         np.array of multiple cohort in shape (amount of cohorts, N_STEPS, 4)
     """
+    
+    coin_data = pd.read_csv(file_name)
+    coin_array = np.array(coin_data.drop(coin_data[['Vol.', 'Date']], axis=1))[:30]
+
+    
     for i in range(0, len(coin_array)-N_STEPS):
         if i == 0:
             test_array = np.array([coin_array[0:N_STEPS]])
@@ -14,3 +20,19 @@ def create_arrays(coin_array, N_STEPS):
             temp_array = np.array([coin_array[i:i+N_STEPS]])
             test_array = np.append(test_array, temp_array, axis=0)
     return test_array
+
+
+def list_of_LSTM_models(model_path):
+    temp_dict = {}
+    file_list = os.listdir(path=model_path)
+    for each in file_list:
+        temp = each.split('_')
+        index_name = f'{temp[3]}_{temp[4]}'
+        if temp_dict.get(index_name) is None:
+            temp_dict[index_name] = [each]
+        else:
+            temp_old_value = temp_dict[index_name]
+            temp_dict[index_name] = [temp_old_value[0], each]
+            
+    return temp_dict
+            
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from const import *
 from func import *
 
-coin_data = pd.read_csv('./DOGE_trade_data__per_1h__20814_items__created_21-Dec-2023_16-07.csv')
-coin_array = np.array(coin_data.drop(coin_data[['Vol.', 'Date']], axis=1))[:30]
-
+## input values:
+file_name = './DOGE_trade_data__per_1h__20814_items__created_21-Dec-2023_16-07.csv'
 N_STEPS = 24
+model_path = './models/'
 
-test_array = create_arrays(coin_array=coin_array, N_STEPS=N_STEPS)
+## step 1: create cohorts
+# test_array = create_arrays(file_name=file_name, N_STEPS=N_STEPS)
 
-# yes
+## step 2: load each model
+model_dict = list_of_LSTM_models(model_path=model_path) 
+
+## step 3: Test each model at different percentages. 
+for each in model_dict.items():
+    print(each)
