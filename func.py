73abe06c68fc416eb627213a6e5a817da9ea02f8
Dennis <dnns.mldr@gmail.com>
2023-12-24 16:23:35 +0100

    yes

--- func.py
+++ func.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 import os
-import json
+import pickle
+import tensorflow as tf
 
-def create_arrays(file_name, N_STEPS):
+def create_arrays(file_name: str, N_STEPS: int):
     """
     Creates a numpy array with multiple N_STEPS-hours cohorts of 4 trade values of a cryptocoin. 
     Returns:
@@ -11,7 +12,7 @@ def create_arrays(file_name, N_STEPS):
     """
     
     coin_data = pd.read_csv(file_name)
-    coin_array = np.array(coin_data.drop(coin_data[['Vol.', 'Date']], axis=1))[:30]
+    coin_array = np.array(coin_data.drop(coin_data[['Vol.', 'Date']], axis=1))[:26]
 
     
     for i in range(0, len(coin_array)-N_STEPS):
@@ -24,6 +25,14 @@ def create_arrays(file_name, N_STEPS):
 
 
 def list_of_LSTM_models(model_path):
+    """create a list of available models and scalers
+
+    Args:
+        model_path (str): location of models/ scalers
+
+    Returns:
+        dict: different models/ scalers
+    """    
     temp_dict = {}
     file_list = os.listdir(path=model_path)
     for each in file_list:
@@ -35,4 +44,28 @@ def list_of_LSTM_models(model_path):
             temp_dict[index_name].update({temp[5]: each})
             
     return temp_dict
-            
+
+        
+def test_models(test_array:np.ndarray, model_dict: dict):
+    key_list = list(model_dict.keys())
+    
+    for each_model in key_list:
+        # loading model
+        file_name_model = './Models/' + model_dict[each_model]['Model']
+        model = tf.keras.models.load_model(file_name_model)
+        # scaler
+        file_name_scaler = './Models/' + model_dict[each_model]['Scaler']
+        with open(file_name_scaler, "rb") as f:
+            scaler = pickle.load(f)
+        
+        # Each cohort
+        for each_cohort in test_array:
+            scaled_array = scaler.fit_transform(each_cohort).reshape(1,24,4)
+            y_pred = model.predict(scaled_array, verbose=0)[0][0]
+            print(y_pred)
+        input_pred = [[y_pred, y_pred, y_pred, y_pred]]
+        #  scaler.inverse_transform(input_pred)[0][0]
+    # except Exception as err:
+    #     log(f'ERROR BALANCE,{coin},1,1,1,1,{err}')
+    #     send_mail(action='Error', stringer=f'GET_CANDLES went wrong: {err}')
+    #     print(err)
--- main.py
+++ main.py
@@ -6,15 +6,16 @@ file_name = './DOGE_trade_data__per_1h__20814_items__created_21-Dec-2023_16-07.c
 N_STEPS = 24
 model_path = './models/'
 
-## step 1: create cohorts
-# test_array = create_arrays(file_name=file_name, N_STEPS=N_STEPS)
+# step 1: create cohorts
+test_array = create_arrays(file_name=file_name, N_STEPS=N_STEPS)
 
-## step 2: load each model
+# step 2: load each model
 model_dict = list_of_LSTM_models(model_path=model_path) 
 
-## step 3: Test each model at different percentages. 
-key_list = list(model_dict.keys())
-print(key_list)
-print(model_dict[key_list[0]].keys())
-print(model_dict[key_list[0]]['Model'])
-print(model_dict[key_list[0]]['Scaler'])
+# step 3: Test each model at different percentages. 
+test_models(test_array=test_array, model_dict=model_dict)
+
+
+
+
+
