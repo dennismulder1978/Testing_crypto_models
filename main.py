da87daaef3d82d29f12fe5e63e9f8afeff42ed90
Dennis Mulder <dnns.mldr@gmail.com>
2023-12-25 11:51:57 +0100

    yeash

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ percentage_list = [0.5, 1, 2, 3]
 
 # step 1: determine avaiability of each model
 model_dict, n_list = list_of_LSTM_models(model_path=model_path) 
-
+print(model_dict)
 # step 2: create cohorts
 test_array_dict = create_arrays(crypto_data_path=crypto_data_path, 
                                 N_STEPS_list=n_list)
